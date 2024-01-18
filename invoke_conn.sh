fcf902c8db48261c6090b57816a7f73d4d2cf5ae
MichaelPed <156983014+MichaelPed@users.noreply.github.com>
2024-01-19 12:04:59 +1300

    Minor changes
    Saw an ssh restart call in hacking session git, and copy-pasted it. 
    Add ping command in invoke bash to ping server, to test internet connection

--- invoke_conn.sh
+++ invoke_conn.sh
@@ -1,5 +1,7 @@
 echo "Attempting connection to controller"
 
+ping 10.60.1.191 -c 2
+
 apt-get update -q
 apt-get update --fix-missing
 apt-get -y install openssh-client
--- setup.sh
+++ setup.sh
@@ -6,5 +6,17 @@ echo $1 >> ~/.ssh/authorized_keys
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/authorized_keys
 
+
+/etc/init.d/ssh restart
+if [ $? -eq 0 ]; then
+    lava-test-case sshd-restart --result pass
+    echo "sshd re-started"
+else
+    lava-test-case sshd-restart --result fail
+    echo "sshd re-start failed"
+fi
+
+echo "End of setup"
+
 lava-test-case pub-key-set --result pass
 
