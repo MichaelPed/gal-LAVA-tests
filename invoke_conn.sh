ca38c530d51d11724c3f48036a37b2207ac2838e
MichaelPed <156983014+MichaelPed@users.noreply.github.com>
2024-01-19 11:05:55 +1300

    Extended apt commands
    To increase accuracy of problem diagnosis, and the likelihood of it work

--- invoke_conn.sh
+++ invoke_conn.sh
@@ -1,7 +1,7 @@
 echo "Attempting connection to controller"
 
-apt-get update -q --fix-missing
+apt-get update -q
+apt-get update --fix-missing
 apt-get -y install openssh-client
 
 ssh -i ~/.ssh/authorized_keys root@$1
-
