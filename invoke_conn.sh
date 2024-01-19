f495e37ee7cdb17f7a59e4995d679bcab9446c3c
MichaelPed <156983014+MichaelPed@users.noreply.github.com>
2024-01-19 13:06:25 +1300

    Minor updates
    Minor updates to setup bash and invoke bash

--- invoke_conn.sh
+++ invoke_conn.sh
@@ -1,9 +1,13 @@
 echo "Attempting connection to controller"
 
-ping 10.60.1.191 -c 2
-
+echo "RUNNING apt-get update -q"
 apt-get update -q
+
+echo "RUNNING apt-get update --fix-missing"
 apt-get update --fix-missing
+
+echo "RUNNING apt-get -y install openssh-client"
 apt-get -y install openssh-client
 
+echo "RUNNING ssh -i ~/.ssh/authorized_keys root@$1"
 ssh -i ~/.ssh/authorized_keys root@$1
